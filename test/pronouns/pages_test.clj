fba4bc253ef9ae8031fc63092b1b646dd52e79ed
Emily A Grasmeder <egrasmed@thoughtworks.com>
2017-07-20 21:26:13 +0200

    add some useless tests as an example

--- test/pronouns/pages_test.clj
+++ test/pronouns/pages_test.clj
@@ -1,5 +1,6 @@
 (ns pronouns.pages-test
   (:require [pronouns.pages :as pages]
+            [clojure.test :refer :all]
             [midje.sweet :refer :all]))
 
 (fact "prose-comma-list turns a list of strings into a prose list with commas"
@@ -8,3 +9,14 @@
       (pages/prose-comma-list ["foo" "bar" "baz"]) => "foo, bar, and baz"
       (pages/prose-comma-list ["foo" "bar" "baz" "bobble"]) => "foo, bar, baz, and bobble"
       (pages/prose-comma-list []) => "")
+
+(deftest pronouns
+  (testing "lol literally nothing"
+    (with-redefs  [pages/pronouns (fn  [args]  args )]
+          (is  (not  (= "1234" (  pages/pronouns "99999"   )  )))
+          (is  (= "1234" (  pages/pronouns "1234"   )  ))))
+;  (testing "lol literally again "
+;    (with-redefs  [pages/format-pronoun-examples
+;                   (fn  [& args]  args )]
+;          (is  (= 1234 ( pages/pronouns {:* "they" "x" "y", "foo" "bar"})))))
+  )
